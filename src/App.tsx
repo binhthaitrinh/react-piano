a13090e5dd813c7e8b4b171aa85a02ca99d254b4
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-02-15 09:52:32 -0800

    add and style logo, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { Logo } from "./components/Logo/Logo";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <main className="app-content" />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
